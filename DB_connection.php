ac93264e6d93603dabc9d5d709bc1a70eed63837
Yusin <yunisaden3@gmail.com>
2024-03-26 18:20:08 +0300

    Update connection String 2

--- DB_connection.php
+++ DB_connection.php
@@ -2,9 +2,9 @@
 
 $sName = "hustehub-server.mysql.database.azure.com";
 $uName = "kuiapqspwm";
-$pass = "AZURE_MYSQL_PASSWORD";
-$db_name = "AZURE_MYSQL_DBNAME";
-$port = "AZURE_MYSQL_PORT";
+$pass = "7072HBLJOAE1SU86$";
+$db_name = "hustehub-database";
+$port = 3306;
 
 try{
     $conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
