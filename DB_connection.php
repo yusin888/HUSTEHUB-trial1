37dac3377567fa608ae46251b2c35a5b06903c2c
Yusin <yunisaden3@gmail.com>
2024-03-26 21:52:12 +0300

    Update connection String 3

--- DB_connection.php
+++ DB_connection.php
@@ -1,18 +1,6 @@
 <?php
 
-$sName = "hustehub-server.mysql.database.azure.com";
-$uName = "kuiapqspwm";
-$pass = "7072HBLJOAE1SU86$";
-$db_name = "hustehub-database";
-$port = 3306;
-
-try{
-    $conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-}
-catch(PDOException $e){
-    echo "Connection failed: ".$e->getMessage();
-    exit;
-}
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "E:\Jkuat units\units 3.2\Commercial Programming\hustehub-server_hustehub-database.pbids", NULL, NULL);
+mysqli_real_connect($conn, "hustehub-server.mysql.database.azure.com", "kuiapqspwm", "7072HBLJ0AE1SU86$", "hustehub-database", 3306, MYSQLI_CLIENT_SSL);
 ?>
